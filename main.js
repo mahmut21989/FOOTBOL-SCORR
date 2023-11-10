3c879fb46cff350ea1866745ef35488ee3c1596a
Mahmut Topcu <topcumhmt68@gmail.com>
2023-11-10 22:51:43 +0300

    update

--- main.js
+++ main.js
@@ -4,7 +4,26 @@ function changeTeamName() {
     document.getElementById("teamName").innerText = newTeamName;
 }
 
-const golPlusBirButton = document.querySelector(".button-one");
+const golMinusBirButton = document.querySelector(".button-one");
+golMinusBirButton.addEventListener("click", function() {
+  document.querySelector("#fname").value--;
+  let audio=document.getElementById("music")
+  audio.play();
+});
+
+const golPlusBirButton = document.querySelector(".button-two");
 golPlusBirButton.addEventListener("click", function() {
   document.querySelector("#fname").value++;
+  let audio=document.getElementById("music-two")
+  audio.play();
 });
+
+function promptForGol() {
+    const golSayisi = prompt("Lütfen gol sayısını girin:");
+    const golSayisiInput = document.getElementById("fname");
+
+    if (golSayisi !== null) {
+        golSayisiInput.value = golSayisi;
+    }
+}
+
