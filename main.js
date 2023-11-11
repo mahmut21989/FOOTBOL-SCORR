b044d9774dfc41e0b51674b084f739aa7968dd71
Mahmut Topcu <topcumhmt68@gmail.com>
2023-11-11 22:09:42 +0300

    update

--- main.js
+++ main.js
@@ -14,8 +14,8 @@ function changeTeamName() {
 }
 
 // Aşağısı +1 gol butonun js
-const golMinusBirButton = document.querySelector(".button-one");
-golMinusBirButton.addEventListener("click", function() {
+const golPlusBirButton = document.querySelector(".button-one");
+golPlusBirButton.addEventListener("click", function() {
   document.querySelector("#fname").value++;
   let audio=document.getElementById("music")
   audio.play();
@@ -35,15 +35,7 @@ golMinusBirButtonTwo.addEventListener("click", function() {
   }
 });
 
-// Bu Kendimizin gol sayısını kullanıcıdan istediğimiz butonun js
-function promptForGol() {
-    const golSayisi = prompt("Lütfen gol sayısını girin:");
-    const golSayisiInput = document.getElementById("fname");
 
-    if (golSayisi !== null) {
-        golSayisiInput.value = golSayisi;
-    }
-}
 
 // Aşağısı +2 gol butonunun js 
 const golTwoBirButton = document.querySelector(".button-four");
@@ -68,14 +60,87 @@ golThreeBirButton.addEventListener("click", function(){
   audio.play();  
 })
 
+// Bu Kendimizin gol sayısını kullanıcıdan istediğimiz butonun js
+function promptForGol() {
+  const golSayisi = prompt("Lütfen gol sayısını girin:");
+  const golSayisiInput = document.getElementById("fname");
+
+  if (golSayisi !== null) {
+      golSayisiInput.value = golSayisi;
+  }
+}
+
 
     // TAKIM-B TAKIMI YANİ SAYFANIN SAĞ TARAFININ JS KODLARI
 
+    // TAKIMIN UKULLANICIDAN İSTENDİĞİ JS KODU
 function changeTeamBName(){
   let newTeamBName=prompt("Yeni Takım Adını Giriniz");
   newTeamBName=newTeamBName.toUpperCase();
   document.getElementById("teamBName").innerText =newTeamBName;
 }
+
+  // GOL +1 YAZAN BUTONUN JS KODLARI
+const golPlusBUtton=document.querySelector(".button-six");
+golPlusBUtton.addEventListener("click", function(){
+  document.querySelector("#pageİnput").value++;
+  pageInput.innerHTML = `<strong>${pageInput.value}</strong>`;
+  let audio=document.getElementById("music")
+  audio.play();
+});
+  
+  // GOL -1 YAZAN BUTON JS KODU
+
+  const golMinusBButton=document.querySelector(".button-seven");
+  golMinusBButton.addEventListener("click", function(){
+    const golSayisiBInput =document.querySelector("#pageİnput");
+    let golSayisiB= parseInt(golSayisiBInput.value);
+    if (golSayisiB > 0) {
+      golSayisiB--;
+      golSayisiBInput.value=golSayisiB
+      let audio=document.getElementById("music-two");
+      audio.play();
+
+    }
+
+  })
+
+  // GOL +2 YAZAN BUTON JS KODLARI
+  const golSecendryButton = document.querySelector(".button-eight");
+  golSecendryButton.addEventListener("click", function(){
+    let valueFive = document.querySelector("#pageİnput").value;
+    valueFive=Number(valueFive);
+    valueFive+=2;
+    document.querySelector("#pageİnput").value=valueFive
+    let audio=document.getElementById("music")
+    audio.play();
+  })
+
+  // Aşağıdaki kod +3 gol butonun js kodu 
+  const golTertiaryButton=document.querySelector(".button-nine");
+  golTertiaryButton.addEventListener("click", function(){
+    let valueSix=document.querySelector("#pageİnput").value;
+    valueSix=Number(valueSix);
+    valueSix+=3;
+    document.querySelector("#pageİnput").value=valueSix
+    let audio=document.getElementById("music")
+    audio.play();
+  })
+
+  
+  // Bu Kendimizin gol sayısını kullanıcıdan istediğimiz butonun js
+
+  function promptBGol(){
+    const golSayisiC=prompt("Gol Sayısını Giriniz:");
+    const golSayisiCInput=document.getElementById("pageİnput");
+
+    if (golSayisiC !==null) {
+      golSayisiCInput.value=golSayisiC;
+    }
+  }
+
+
+
 
 
 
